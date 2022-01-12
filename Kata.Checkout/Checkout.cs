9b4c3e986cbfc46f05195d3eaee697a4cb0c380a
Martin Pickering <martin.pickering@maginus.com>
2022-01-12 15:05:28 +0000

    Refactored Checkout to support a Pricer Service so that the failing tests can be persuaded to pass.
    This has broken the build in respect of the Unit Tests.

--- Kata.Checkout/Checkout.cs
+++ Kata.Checkout/Checkout.cs
@@ -6,17 +6,28 @@
 public class Checkout : ICheckout
 {
     private readonly List<CheckoutItem> _items;
+    private readonly IPricer _pricerSvc;
 
-    public Checkout()
+    public Checkout(IPricer pricer)
     {
         _items = new List<CheckoutItem>();  
+        _pricerSvc = pricer;
     }
 
     public decimal Total => _items.Sum(i => i.TotalValue);
 
     public void ScanItem(string SKU)
     {
-        _items.Add(new CheckoutItem(SKU, 1, 10.00M));
+        var item = _items.SingleOrDefault(i => i.SKU == SKU);
+        if (item is null)
+        {
+            _items.Add(new CheckoutItem(SKU, 1, _pricerSvc.PriceItem(SKU, 1)));
+            return;
+        }
+        var newQty = item.Quantity + 1;
+        var newItem = new CheckoutItem(SKU, newQty, _pricerSvc.PriceItem(SKU, newQty));
+        _items.Remove(item);
+        _items.Add(newItem);
     }
 }
 
---
+++ Kata.Checkout/IPricer.cs
@@ -0,0 +1,15 @@
+﻿namespace Kata.Checkout;
+
+/// <summary>
+/// The contract for an Item Pricing Service
+/// </summary>
+public interface IPricer
+{
+    /// <summary>
+    /// Price an Item.
+    /// </summary>
+    /// <param name="sku">The SKU of the Item to be priced</param>
+    /// <param name="qty">The number of the Items so that quantity break pricing can be evaluated</param>
+    /// <returns>The total price for all of the Items</returns>
+    decimal PriceItem(string sku, uint qty);
+}
