40a3a6ba779e5c050f77f12405da002b7df54086
Martin Pickering <martin.pickering@maginus.com>
2022-01-12 14:47:45 +0000

    Updated Item Scanning Unit Tests to enrich the set of test cases.

--- Kata.Checkout.UnitTest/ItemScanningTests.cs
+++ Kata.Checkout.UnitTest/ItemScanningTests.cs
@@ -4,6 +4,10 @@ using Xunit;
 /// <summary>
 /// A set of tests focused on the behaviour of item scanning
 /// </summary>
+/// <remarks>One issue relating to the Checkout.ScanItem method 
+/// is it is not in itself very testable. In respect of this simple 
+/// Kata it probably does not matter. But as things grow beyond the 
+/// present scope the lack of testability could become a headache.</remarks>
 public class ItemScanningTests
 {
     [Fact]
@@ -13,4 +17,23 @@ public class ItemScanningTests
 
         checkout.ScanItem("123456");
     }
+
+    [Fact]
+    public void CanScanMultipleOfTheSameItem()
+    {
+        var checkout = new Checkout();
+
+        checkout.ScanItem("123456");
+        checkout.ScanItem("123456");
+    }
+
+    [Fact]
+    public void CanScanMultipleDifferentItems()
+    {
+        var checkout = new Checkout();
+
+        checkout.ScanItem("123456");
+        checkout.ScanItem("789012");
+    }
+
 }
