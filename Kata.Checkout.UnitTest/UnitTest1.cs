dea27302d397ff32495b3382433b5d4575b55242
Martin Pickering <martin.pickering@maginus.com>
2022-01-12 13:59:34 +0000

    Added Projects (from VS Template)
    .Net Class Library and .Net XUnit Test Projects.
    Also, updated the MSTest SDK Nuget.

---
+++ Kata.Checkout.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Kata.Checkout.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Kata.Checkout/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Kata.Checkout
+{
+    public class Class1
+    {
+
+    }
+}
