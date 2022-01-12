30f3889f104b4ff32f57a26fa5ef926490d959b1
Martin Pickering <martin.pickering@maginus.com>
2022-01-12 14:53:13 +0000

    Added more Checkout Total Tests
    NOTE: the Test Suite is back to failing again.

--- Kata.Checkout.UnitTest/CheckoutTotalTests.cs
+++ Kata.Checkout.UnitTest/CheckoutTotalTests.cs
@@ -17,4 +17,34 @@ public class CheckoutTotalTests
 
         Assert.NotEqual(initialTotal, actualTotal);
     }
+
+    [Fact]
+    public void ScanningSeveralOfAnItemWithPriceUpdatesTotal()
+    {
+        var checkout = new Checkout();
+        checkout.ScanItem("123456");
+        var unitPrice = checkout.Total;
+        var expectedTotal = unitPrice * 3;
+
+        checkout.ScanItem("123456");
+        checkout.ScanItem("123456");
+        var actualTotal = checkout.Total;
+
+        Assert.Equal(expectedTotal, actualTotal);
+    }
+
+    [Fact]
+    public void ScanningSeveralOfAnItemWithQuantityBreakPriceUpdatesTotal()
+    {
+        var checkout = new Checkout();
+        checkout.ScanItem("123456");
+        var unitPrice = checkout.Total;
+        var straightMultiple = unitPrice * 3;
+
+        checkout.ScanItem("123456");
+        checkout.ScanItem("123456");
+        var actualTotal = checkout.Total;
+
+        Assert.NotEqual(straightMultiple, actualTotal);
+    }
 }
